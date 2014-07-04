f2ee46c4de78c21489da9ac326ee3755ccb06feb
Hao Wu <hwu@intellisurvey.com>
2014-07-04 09:49:28 -0700

    tidy

--- word_dancer.pl
+++ word_dancer.pl
@@ -6,19 +6,21 @@ use FindBin qw($Bin);
 # the sad thing is you don know how how many of it
 
 my @words;
-# replace absolute address with relative one 
 
-get '/' => sub { 
-    tie @words, 'Tie::File', "$Bin/word.txt" 
-    or die "can not find file word.txt"; 
-        template 'show_words.tt', {
-            'entries'     => [ reverse @words ],
-            'add_word_url' => uri_for('add'),
-        } 
+# replace absolute address with relative one
+
+get '/' => sub {
+    tie @words, 'Tie::File', "$Bin/word.txt"
+      or die "can not find file word.txt";
+    template 'show_words.tt',
+      {
+        'entries'      => [ reverse @words ],
+        'add_word_url' => uri_for('add'),
+      };
 };
 
 post '/add' => sub {
-    push @words, params->{word} if params->{word} =~/\w/;
+    push @words, params->{word} if params->{word} =~ /\w/;
     untie @words;
     redirect '/';
 };
